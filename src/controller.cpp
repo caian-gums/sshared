30645b31e4fef42e2be4d2609c377f639c79315f
Ilê Caian <ile.caian@gmail>
2016-12-21 12:13:04 -0200

    args verification on controller

--- src/controller.cpp
+++ src/controller.cpp
@@ -2,14 +2,23 @@
 
 std::string Controller::filter_message(char* mes[], int size) {
 
-    int i = 1;
-    std::string out;
-    while(i < size) {
-        out += "mes[" + std::to_string(i) + "]: " + mes[i] + "\n";
-        i++;
+    // return value
+    std::string rv;
+    // check for number of arguments
+    if(size % 2 == 0 || size < 2) {
+        rv = "Error on usage. Number of arguments incorrect";
+        return rv;
     }
-    out = "size: " + std::to_string(size) + "\n" + out;
+
+    int i = 1;
+    while(i < size) {
+        rv += "mes[" + std::to_string(i) + "]: " + mes[i] + "\n";
+        rv += "mes[" + std::to_string(i) + "]: " + mes[i+1] + "\n";
+        // two positions
+        i = i + 2;
+    }
+    rv = "size: " + std::to_string(size) + "\n" + rv;
     
-    return out;
+    return rv;
 
 }
--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
 echo "# sshared tests\n"
 
-echo "[test] filter message..."
+echo "[test] incorrect filter message..."
+./sshared -in my.file -n  > output
+echo " - result - "
+cat output
+
+echo "\n[test] correct filter message..."
 ./sshared -in my.file -n 5 -t 3 > output
 echo " - result - "
 cat output

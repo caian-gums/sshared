09731e41b8c319f118c37b713baad72a03da12b0
Ilê Caian <ile.caian@gmail.com>
2017-01-09 22:56:32 -0200

    list implemented with tests for std::string and integer

---
+++ include/list.h
@@ -0,0 +1,56 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <vector>
+
+template <class T>
+
+class List {
+public:
+
+    // Constructor
+    List();
+    ~List();
+
+    /* add method adds the given element el on the
+     * list
+     *
+     *  @param  el      element to be inserted
+     *
+     *  @return void
+     */
+    void add(T el);
+
+    /* remove method remove the last inserted element
+     * 
+     *  @return void
+     */
+    void remove();
+
+    /* len method return the size of the list
+     *
+     *  @return the size of the list
+     */
+    unsigned int len();
+
+    /* get method get the reference to the given element
+     * on specific position
+     *
+     *  @param  pos     position of the element on
+     *                  the list
+     * 
+     *  @return the element or null
+     */
+    T get(unsigned int pos);
+
+private:
+    // attributes
+    // inside list
+    std::vector<T> list;
+
+};
+
+// List template implementation
+#include "list.tpp"
+
+#endif
